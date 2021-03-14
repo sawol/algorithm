07330e64647fb71020c46ef591e60d25b1505bde
sawol <55649302+sawol@users.noreply.github.com>
2021-03-14 18:32:25 +0900

    Delete 가장 긴 증가하는 부분 수열.py

--- Baekjoon/Dynamic programming/가장 긴 증가하는 부분 수열.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-arr = [int(i) for i in input().split()]
-dp = [1]*N
-for p in range(1, N):
-    for q in range(p):
-        if arr[p] > arr[q]:
-            dp[p] = max(dp[p], dp[q]+1)
-print(max(dp))
