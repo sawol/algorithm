df135760a48a9ba9729fd4e1bb59133d37afae9b
sawol <55649302+sawol@users.noreply.github.com>
2021-03-25 17:34:07 +0900

    Update 알파벳 개수

--- Baekjoon/알파벳 개수.py
+++ Baekjoon/알파벳 개수.py
@@ -2,5 +2,9 @@ import string
 arr = [0]*26
 alp = list(string.ascii_lowercase)
 s = input()
-for i in range(26):
-    print(s.count(alp[i]), end=' ')
+for i in s:
+    arr[alp.index(i)] += 1
+​
+for p in arr:
+  print(p, end = ' ')
+​
