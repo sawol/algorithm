5c8a3a10ac56d437103237ff31c3e65ca43352a9
sawol <55649302+sawol@users.noreply.github.com>
2021-04-01 22:58:58 +0900

    Update 두 수의 합.py

--- Baekjoon/두 수의 합.py
+++ Baekjoon/두 수의 합.py
@@ -1,13 +1,8 @@
-left, right = 0, int(input()) - 1
-arr = sorted(list(map(int, input().split())))
+n = input()
+arr = set(map(int, input().split()))
 x = int(input())
 cnt = 0
-while left != right:
-    if arr[left] + arr[right] > x:
-        right -= 1
-    elif arr[left] + arr[right] == x:
+for i in arr:
+    if x - i in arr:
         cnt += 1
-        right -= 1
-    elif arr[left] + arr[right] < x:
-        left += 1
-print(cnt)
+print(cnt//2)
