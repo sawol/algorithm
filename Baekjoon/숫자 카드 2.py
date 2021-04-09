199fa22cf4ff268a8b9fcb3e01fab009d49de4ee
sawol <55649302+sawol@users.noreply.github.com>
2021-04-09 18:22:52 +0900

    Update 숫자 카드 2.py

--- Baekjoon/숫자 카드 2.py
+++ Baekjoon/숫자 카드 2.py
@@ -1,13 +1,9 @@
 import collections
 
-hash = collections.defaultdict(int)
 N = int(input())
-s:list = list(map(int, input().split()))
+hash = collections.Counter(list(map(int, input().split())))
 M = int(input())
 j:list = list(map(int, input().split()))
-
-for char in s:
-    hash[char] += 1
 
 for char in j:
     print(hash[char], end=' ')
