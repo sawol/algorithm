8f6349fb1fa4a2022ca4c2042ba38d7138e18ea1
sawol <55649302+sawol@users.noreply.github.com>
2021-04-09 18:22:43 +0900

    Update 숫자 카드 2.py

--- Baekjoon/숫자 카드 2.py
+++ Baekjoon/숫자 카드 2.py
@@ -1,17 +1,13 @@
-hash = {}
+import collections
+
+hash = collections.defaultdict(int)
 N = int(input())
 s:list = list(map(int, input().split()))
 M = int(input())
 j:list = list(map(int, input().split()))
 
 for char in s:
-    if char in hash:
-        hash[char] += 1
-    else:
-        hash[char] = 1
+    hash[char] += 1
 
 for char in j:
-    if char in hash:
-        print(hash[char], end=' ')
-    else:
-        print(0, end=' ')
+    print(hash[char], end=' ')
