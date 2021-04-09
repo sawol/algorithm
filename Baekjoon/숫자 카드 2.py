478aa4e184d0ccf2045785cec202b502edf60713
sawol <55649302+sawol@users.noreply.github.com>
2021-04-09 17:46:40 +0900

    Create 숫자 카드 2.py

---
+++ Baekjoon/숫자 카드 2.py
@@ -0,0 +1,18 @@
+hash = {}
+cnt = 0
+N = int(input())
+s:list = list(map(int, input().split()))
+M = int(input())
+j:list = list(map(int, input().split()))
+
+for char in s:
+    if char in hash:
+        hash[char] += 1
+    else:
+        hash[char] = 1
+
+for char in j:
+    if char in hash:
+        print(hash[char], end=' ')
+    else:
+        print(0, end=' ')
