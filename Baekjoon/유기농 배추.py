34c7fd73828ada1b3abdba4a74498d75063a5095
sawol <55649302+sawol@users.noreply.github.com>
2021-04-12 21:10:36 +0900

    Update 유기농 배추.py

--- Baekjoon/유기농 배추.py
+++ Baekjoon/유기농 배추.py
@@ -1,24 +1,31 @@
+import sys
+
+sys.setrecursionlimit(10000)
 T = int(input())
-     
+def dfs(field, i:int, j:int):
+    # 밭의 범위에서 벗어나면 종료
+    if i < 0 or j <0 or i >= N or j >= M or field[i][j] != 1:
+        return
+    # 이미 방문한 곳은 0으로 마킹
+    field[i][j] = 0
+    
+    # 상하좌우 탐색
+    dfs(field, i+1, j)
+    dfs(field, i-1, j)
+    dfs(field, i, j+1)
+    dfs(field, i, j-1)
+        
 for _ in range(T):
     M, N, K = map(int, input().split())
     field = [[0]*M for _ in range(N)]
-    
+    cnt = 0
     for _ in range(K):            # 배추 위치 -> 그래프 그리기
         x, y = map(int, input().split())
         field[y][x] = 1
-    stack = []
-    cnt = 0
     for i in range(N):
         for j in range(M):
             if field[i][j] == 1:
-                stack.append((i,j))
+                dfs(field, i, j)
                 cnt += 1
-                while stack:    # 상하좌우 탐색
-                    x, y = stack.pop()
-                    field[x][y] = 0
-                    if x > 0 and field[x-1][y]: stack.append((x-1, y))
-                    if x < N-1 and field[x+1][y]: stack.append((x+1, y))
-                    if y > 0 and field[x][y-1]: stack.append((x, y-1))
-                    if y < M-1 and field[x][y+1]: stack.append((x, y+1))
-    print(cnt)    
+                
+    print(cnt)   
