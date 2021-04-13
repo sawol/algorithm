188937c404e713c09edf8b3b43381d8b2d9a896b
sawol <55649302+sawol@users.noreply.github.com>
2021-04-13 22:53:00 +0900

    Update 모든 순열.py

--- Baekjoon/모든 순열.py
+++ Baekjoon/모든 순열.py
@@ -1,6 +1,19 @@
-import itertools
-result = list(itertools.permutations(list(range(1, int(input())+1))))
+nums = list(range(1, int(input())+1))
+result = []
+prev_elements = []
+def dfs(elements):
+    if len(elements) == 0:
+        result.append(prev_elements[:])
+    
+    for e in elements:
+        next_elements = elements[:]
+        next_elements.remove(e)
+        prev_elements.append(e)
+        
+        dfs(next_elements)
+        prev_elements.pop()
+dfs(nums)
 for i in result:
     for j in i:
-        print(j, end=' ')
+        print(j, end = ' ')
     print()
