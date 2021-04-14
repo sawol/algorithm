d662443b105c096b7b302e17d220f4ec1a412668
sawol <55649302+sawol@users.noreply.github.com>
2021-04-15 00:46:48 +0900

    Update 연결 요소의 개수.py

--- Baekjoon/연결 요소의 개수.py
+++ Baekjoon/연결 요소의 개수.py
@@ -10,16 +10,18 @@ for _ in range(M):
     graph[a].append(b)
     graph[b].append(a)
     
-def recursive_dfs(v):
-    discovered.append(v)
-    for w in graph[v]:
+def stack_dfs(v):
+    stack = [v]
+    while stack:
+        w = stack.pop()
         if not w in discovered:
-            recursive_dfs(w)
-    return discovered
+            discovered.append(w)
+            for x in graph[w]:
+                stack.append(x)
 
 cnt = 0
 for i in range(1, N+1):
     if not i in discovered:
-        recursive_dfs(i)
+        stack_dfs(i)
         cnt += 1
 print(cnt)
